c3d3e169fdd05766ae901b692be1fc86d03aa1b5
Zhongming Chen <ZhongmingC@asos.com>
2018-07-26 20:26:31 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var max = CalculateWords(".");
+
+            Console.Write(max);
+        }
+
+        private static int  CalculateWords(string text)
+        {
+            var sentences = text.Split(new[] { '.', '?', '!' });
+
+            var words = sentences
+                .Where(sentence => !string.IsNullOrWhiteSpace(sentence))
+                .Select(sentence => Regex.Split(sentence.Trim(), @"\s+"));
+
+            return words.Count() > 0 ? words.Max(word => word.Count()) : 0;
+
+        }
+    }
+}
